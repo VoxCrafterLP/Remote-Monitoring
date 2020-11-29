be0efcec844887ef31bbe1a3ba8ffd79fcc2b6f5
BuildTools <lenny.angst@easyid.ch>
2020-11-29 16:02:09 +0100

    Add languages support Add config support Start index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'php/Locales.php';
+
 session_start();
 
 if (isset($_POST['user'])) {
@@ -7,3 +9,28 @@ if (isset($_POST['user'])) {
     exit();
 }
 
+$locales = new Locales();
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Remote Monitoring | Login</title>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+    <div class="login-container">
+        <div class="login-content">
+            <span>SPAN</span>
+        </div>
+    </div>
+
+</body>
+</html>
